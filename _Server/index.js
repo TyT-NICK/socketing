f6c53357e3c0f408302774d985522bde51952372
TyT-NICK <artem99k14@gmail.com>
2021-01-11 02:07:21 +0800

    added simple socketing

--- _Server/index.js
+++ _Server/index.js
@@ -1,7 +1,27 @@
-const app = require('express')()
+const app   = require('express')()
+const http  = require('http').Server(app)
+const io    = require('socket.io')(http)
 
 const PORT = 8080
 
-app.listen(PORT, () => {
+const connectedClients = []
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (client) => {
+  console.log('client connected')
+  connectedClients.push(client) 
+
+  client.on('message-sent', (msg) => {
+    console.log(msg)
+    if (msg) {
+      connectedClients.map(client => client.emit('chat-message', msg))
+    }
+  })
+})
+
+http.listen(PORT, () => {
   console.log(`app at ${PORT}`)
 })
